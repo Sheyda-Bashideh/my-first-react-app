ec2497f19967a925363236c92c4f6ae15d7b502b
Sheyda-Bashide/2020 <sheyda.bsh1994sh@gmail.com>
2025-10-22 14:42:33 +0330

    first commit

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from "./footer.module.css"
+
+function Footer() {
+  return (
+    <>
+   
+    <h2 className={style.myfooter}>Footer</h2>
+    </>
+  )
+}
+
+export default Footer
---
+++ components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from "./header.module.css"
+
+function Header() {
+  return (
+    <>
+   
+    <h2  className={style.myheader}>Hello jsx</h2>
+    
+    </>
+  )
+}
+
+export default Header
